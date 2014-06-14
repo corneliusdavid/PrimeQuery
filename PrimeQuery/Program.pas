aebf9379d4783ebcec4694a4272a78402819f587
David Cornelius <dev@corneliusconcepts.com>
2014-06-13 21:58:19 -0700

    initial project
    a console program to test whether an entered number is prime or no

---
+++ PrimeQuery/Program.pas
@@ -0,0 +1,30 @@
+﻿namespace PrimeQuery;
+
+interface
+
+
+type
+  ConsoleApp = class
+  public
+    class method Main(args: array of String);
+  end;
+
+implementation
+
+
+class method ConsoleApp.Main(args: array of String);
+var
+  i: Integer;
+begin
+  Console.WriteLine('Prime Numbers');
+  for i := 1 to 599 do
+    if PrimeNumberQuery.IsPrime(i) then begin
+      if i > 2 then
+        Console.Write(', ');
+      Console.Write(i.ToString);
+    end;
+
+  Console.ReadLine;
+end;
+
+end.
---
+++ PrimeQueryClass/PrimeQueryClass.pas
@@ -0,0 +1,19 @@
+﻿namespace PrimeQueryWPF;
+
+interface
+
+uses
+  System.Collections.Generic,
+  System.Linq,
+  System.Text;
+
+type
+  Class1 = public class
+  private
+  protected
+  public
+  end;
+
+implementation
+
+end.
