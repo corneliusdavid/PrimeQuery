97258b583ee16047a1f9bbb947f0fe7e8284c1f2
corneliusdavid <dev@corneliusconcepts.com>
2015-02-15 14:52:07 -0800

    switched to unsigned int 64 and set the max number that will be processed by this class; also optimized the IsPrime calculation a little based on some reading on the internet

--- PrimeShared/PrimeNumberQuery.pas
+++ PrimeShared/PrimeNumberQuery.pas
@@ -8,16 +8,18 @@ type
     method GetAsString: String;
     method SetAsString(NumStr: String);
   public
-    class method IsPrime(const TestNumber: Integer): Boolean;
+    class method IsPrime(const TestNumber: UInt64): Boolean;
     method Check: Boolean;
-    property Number: Integer read write;
+    property Number: UInt64 read write;
     property AsString: String read GetAsString write SetAsString;
+  public invariants
+    Number < 4000000000000;
   end;
 
   PrimeNumberEdit = public class(PrimeNumberQuery)
   public
     method Clear;
-    method AddDigit(const NewDigit: Integer);
+    method AddDigit(const NewDigit: UInt64);
     method Backspace;
     method IncNumber;
     method DecNumber;
@@ -27,23 +29,24 @@ implementation
 
 { PrimeNumberQuery }
 
-class method PrimeNumberQuery.IsPrime(const TestNumber: Integer): Boolean;
+class method PrimeNumberQuery.IsPrime(const TestNumber: UInt64): Boolean;
 var 
-  i: Integer := 3;
-  max: Integer := Integer(Math.Round(Math.Sqrt(Double(TestNumber))));
+  max: UInt64 := UInt64(Math.Round(Math.Sqrt(Double(TestNumber))));
 begin
-  if TestNumber = 2 then
-    Result := True
-  else if (TestNumber < 2) or (TestNumber mod 2 = 0) then
+  // handle 2 and 3, false for numbers less than 2
+  if TestNumber <= 3 then
+    Result := TestNumber > 1
+
+  // handle multiples of 2 and 3
+  else if (TestNumber mod 2 = 0) or (TestNumber mod 3 = 0) then
     Result := False
   else begin
     Result := True;
-    while i < max do
-      if TestNumber mod i = 0 then begin
+    for i: Integer := 5 to max step 6 do
+      if (TestNumber mod i = 0) or (TestNumber mod (i + 2) = 0) then begin
         Result := False;
         Break;
-      end else
-        i := i + 2;
+      end;
   end;
 end;
 
@@ -59,7 +62,7 @@ end;
 
 method PrimeNumberQuery.SetAsString(NumStr: String);
 begin
-  Integer.TryParse(NumStr, out Number);
+  UInt64.TryParse(NumStr, out Number);
 end;
 
 { PrimeNumberEdit }
@@ -77,7 +80,7 @@ begin
     Number := 0;
 end;
 
-method PrimeNumberEdit.AddDigit(NewDigit: Integer);
+method PrimeNumberEdit.AddDigit(NewDigit: UInt64);
 begin
   if Number > 0 then
     SetAsString(Number.ToString + NewDigit.ToString)
